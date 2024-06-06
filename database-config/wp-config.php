6ea885734661d549b1441fbb8e407259b5894b04
Guycho980 <94019151+Guycho980@users.noreply.github.com>
2024-06-06 19:35:29 -0400

    Update wp-config.php

--- database-config/wp-config.php
+++ database-config/wp-config.php
@@ -29,7 +29,7 @@ define( 'DB_USER', 'root' );
 define( 'DB_PASSWORD', 'admin' );
 
 /** Database hostname */
-define( 'DB_HOST', '34.136.146.43' );
+define( 'DB_HOST', '10.108.160.3' );
 
 /** Database charset to use in creating database tables. */
 define( 'DB_CHARSET', 'utf8' );
